8fcc1e026f8d01fd3a84978b27e3b83f21950c29
Robert Eklund <robert.eklund3@gmail.com>
2018-11-04 20:56:48 -0600

    push up remaining items

--- Models/Game.cs
+++ Models/Game.cs
@@ -9,6 +9,7 @@ namespace YourGameOfTheYear.Models
         [Key]
         public int ID { get; private set; }
         public string GamgName { get; set; }
+        public string GameDescription { get; set; }
         public List<Genre> Genre { get; set; }
         public string Studio { get; set; }
         public double GameRating { get; set; }
--- Models/Genres.cs
+++ Models/Genres.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace YourGameOfTheYear.Models
 {
     public class Genre
     {
+        [Key]
         public int ID { get; set; }
         public string GenreName { get; set; }
     }
