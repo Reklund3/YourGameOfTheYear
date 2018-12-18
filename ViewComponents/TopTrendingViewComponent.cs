abd436fbd8e0bf4b204a4b9d4408d46b2a2243a7
Robert Eklund <robert.eklund3@gmail.com>
2018-12-17 22:14:46 -0600

    Add the Top Trending View Component and align rows

---
+++ ViewComponents/TopTrendingViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YourGameOfTheYear.Data;
+using YourGameOfTheYear.Models;
+
+namespace YourGameOfTheYear.ViewComponents
+{
+    public class TopTrendingViewComponent : ViewComponent
+    {
+        private readonly YourGameOfTheYearContext _context;
+        public TopTrendingViewComponent(YourGameOfTheYearContext context)
+        {
+            this._context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Game.OrderByDescending(x => x.GameRating).Take(10).Select(game => new TopTen { GameName = game.GamgName, Rating = game.GameRating }));
+        }
+    }
+}
