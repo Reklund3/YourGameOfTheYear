a267ca5ccba8a75c0ff3d41cec5af422b5a91691
Robert Eklund <robert.eklund3@gmail.com>
2018-12-20 23:09:05 -0600

    start adding the trending games model and hosted service to check/update the trending list

---
+++ Models/TrendingGames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourGameOfTheYear.Models
+{
+    public class TrendingGames
+    {
+        public string GameName { get; set; }
+        public int GameActivity { get; set; }
+    }
+}
